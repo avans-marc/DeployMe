c7a586356f8f4935535f87833b041cee406c6eea
Marc Mathijssen <maa.mathijssen@avans.nl>
2024-10-05 16:59:21 +0200

    use azure credentials instead of publishing profile

--- DeployMe.API/Program.cs
+++ DeployMe.API/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<StudentDbContext>(options =>
 
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,18 +22,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
